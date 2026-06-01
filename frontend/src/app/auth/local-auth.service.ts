bf46829a812c94298d44467593e0708d3997128f
Momchil Momchilov <momchil.momchilov@convesys.com>
2026-06-02 00:27:30 +0300

    Update current user on local auth changes

--- frontend/src/app/auth/local-auth.service.ts
+++ frontend/src/app/auth/local-auth.service.ts
@@ -2,7 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable, tap } from 'rxjs';
+import { UserAdapter } from '../data/adapters/_user-adapter';
 import { User_DTO } from '../data/dtos';
+import { UsersService } from '../data/services/users.service';
 
 interface LoginResponse {
   token: string;
@@ -20,6 +22,7 @@ export class LocalAuthService {
   constructor(
     private http: HttpClient,
     private router: Router,
+    private usersService: UsersService,
   ) {}
 
   get token(): string | null {
@@ -34,12 +37,14 @@ export class LocalAuthService {
     return this.http.post<LoginResponse>(`${baseUrl}/auth/login`, { username, password }).pipe(
       tap((response) => {
         localStorage.setItem(authTokenStorageKey, response.token);
+        this.usersService.setCurrentUser(UserAdapter.dtoToModel(response.user));
       }),
     );
   }
 
   logout(): void {
     localStorage.removeItem(authTokenStorageKey);
+    this.usersService.setCurrentUser(undefined);
     this.router.navigate(['/login']);
   }
 }
